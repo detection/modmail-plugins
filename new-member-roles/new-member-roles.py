3701b0036b5613e00816b9f1cb710e7a62f9b5e3
detection <big.machine@gmail.com>
2020-07-14 19:12:10 -0400

    Update new-member-roles.py

--- new-member-roles/new-member-roles.py
+++ new-member-roles/new-member-roles.py
@@ -12,12 +12,12 @@ class NewMemberRoles(commands.Cog):
     @commands.command(name="approve", aliases=["app"])
     @checks.has_permissions(PermissionLevel.MODERATOR)
     async def approve(self, ctx):
-        mem = ctx.thread.recipient.id
+        member = ctx.thread.recipient
         pend = ctx.guild.get_role(324658636574162945)
         nego = ctx.guild.get_role(732405243299495968)
         madrid = ctx.guild.get_role(305440616354152450)
-        await member.add_roles(mem, nego, madrid)
-        await member.remove_roles(mem, pend)
+        await member.add_roles(nego, madrid)
+        await member.remove_roles(pend)
         
         config = await self.db.find_one({"_id": "config"})
 
