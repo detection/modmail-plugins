2c44b6458ac429c3bd57497e998519e2aea570e6
detection <big.machine@gmail.com>
2020-07-14 17:06:47 -0400

    Update new-member-roles.py

--- new-member-roles/new-member-roles.py
+++ new-member-roles/new-member-roles.py
@@ -1,4 +1,9 @@
+import discord
+import typing
 from discord.ext import commands
+
+from core import checks
+from core.models import PermissionLevel
 
 class NewMemberRoles(commands.Cog):
     def __init__(self, bot):
