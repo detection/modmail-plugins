a9c3c41744e176ce7f47be73a615a8875235eecd
detection <big.machine@gmail.com>
2020-07-14 18:05:16 -0400

    Update new-member-roles.py

--- new-member-roles/new-member-roles.py
+++ new-member-roles/new-member-roles.py
@@ -16,8 +16,8 @@ class NewMemberRoles(commands.Cog):
         pend = ctx.guild.get_role(324658636574162945)
         nego = ctx.guild.get_role(732405243299495968)
         madrid = ctx.guild.get_role(305440616354152450)
-        await client.add_roles(member, nego, madrid)
-        await client.remove_roles(member, pend)
+        await member.add_roles(nego, madrid)
+        await member.remove_roles(pend)
         
         config = await self.db.find_one({"_id": "config"})
 
