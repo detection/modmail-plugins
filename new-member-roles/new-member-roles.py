3b93a16268097b4988badb5ee7f299a17867113c
detection <big.machine@gmail.com>
2020-07-14 16:50:56 -0400

    Update new-member-roles.py

--- new-member-roles/new-member-roles.py
+++ new-member-roles/new-member-roles.py
@@ -4,18 +4,19 @@ class NewMemberRoles(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
-    @commands.command(name="newmemberroles", aliases=["approve"])
+    @commands.command(name="approve", aliases=["app"])
     @checks.has_permissions(PermissionLevel.MODERATOR)
     async def approve(self, ctx):
-        pend = discord.utils.get(memberafter.server.roles, name="Pending")
-        nego = discord.utils.get(memberafter.server.roles, name="Negotiating")
-        madrid = discord.utils.get(memberafter.server.roles, name="Madridistas!")
-        await client.add_roles(nego, madrid)
-        await client.remove_roles(pend)
+        member = """USER ID FROM MODMAIL CHANNEL"""
+        pend = discord.utils.get(ctx.server.roles, name="Pending")
+        nego = discord.utils.get(ctx.server.roles, name="Negotiating")
+        madrid = discord.utils.get(ctx.server.roles, name="Madridistas!")
+        await client.add_roles(member, nego, madrid)
+        await client.remove_roles(member, pend)
         
         config = await self.db.find_one({"_id": "config"})
 
-        await ctx.send("Successfully approved.")
+        await ctx.send("{member} has been successfully approved!")
 
 def setup(bot):
     bot.add_cog(NewMemberRolesPlugin(bot))
