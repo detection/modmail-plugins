347fa75f2a83d42efb8235f2bea5d0d97edff137
detection <big.machine@gmail.com>
2020-07-14 14:21:33 -0400

    Update kick-pending.py

--- kick-pending/kick-pending.py
+++ kick-pending/kick-pending.py
@@ -32,50 +32,48 @@ class KickPending(commands.Cog):
     @commands.command(name="kick", aliases=["getout"])
     @checks.has_permissions(PermissionLevel.MODERATOR)
     async def kick(self, ctx):
-      role = ctx.guild.get_role(732405243299495968)
-      for x in role.members:
-          await x.kick(reason="Inactivity")
-      await ctx.send("Kicked all members with role Negotiating")
+        role = ctx.guild.get_role(732405243299495968)
+        for x in role.members:
+            await x.kick(reason="Inactivity")
+            await ctx.send("Kicked all members with role Negotiating")
         
-      config = await self.db.find_one({"_id": "config"})
+        config = await self.db.find_one({"_id": "config"})
 
-      if config is None:
-          return await ctx.send("There's no configured log channel.")
-      else:
-          channel = ctx.guild.get_channel(int(config["channel"]))
+        if config is None:
+            return await ctx.send("There's no configured log channel.")
+        else:
+            channel = ctx.guild.get_channel(int(config["channel"]))
 
-      if channel is None:
-          await ctx.send("There is no configured log channel.")
-          return
-
-      try:
-          for member in members:
-              await member.kick(reason=f"{reason if reason else None}")
-              embed = discord.Embed(
-                  color=discord.Color.red(),
-                  title=f"{member} was kicked!",
-                  timestamp=datetime.datetime.utcnow(),
-              )
-
-              embed.add_field(
-                  name="Moderator", value=f"{ctx.author}", inline=False,
-              )
-
-              if reason is not None:
-                  embed.add_field(name="Reason", value=reason, inline=False)
-
-              await ctx.send(f"🦶 | {member} is kicked!")
-              await channel.send(embed=embed)
-
-       except discord.Forbidden:
-            await ctx.send("I don't have the proper permissions to kick people.")
-
-       except Exception as e:
-            await ctx.send(
-                "An unexpected error occurred, please check the Heroku logs for more details."
-            )
-            logger.error(e)
+        if channel is None:
+            await ctx.send("There is no configured log channel.")
             return
+
+        try:
+            for member in members:
+                await member.kick(reason=f"{reason if reason else None}")
+                embed = discord.Embed(
+                    color=discord.Color.red(),
+                    title=f"{member} was kicked!",
+                    timestamp=datetime.datetime.utcnow(),
+                )
+
+                embed.add_field(
+                    name="Moderator", value=f"{ctx.author}", inline=False,
+                )
+
+                if reason is not None:
+                    embed.add_field(name="Reason", value=reason, inline=False)
+
+                    await ctx.send(f"🦶 | {member} is kicked!")
+                    await channel.send(embed=embed)
+
+                    except discord.Forbidden:
+                        await ctx.send("I don't have the proper permissions to kick people.")
+
+                    except Exception as e:
+                        await ctx.send("An unexpected error occurred, please check the Heroku logs for more details.")
+                        logger.error(e)
+                        return
             
 def setup(bot):
     bot.add_cog(KickPending(bot))
