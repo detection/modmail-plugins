1762e4d0c41bd370229d2755833dba9088c3e845
detection <big.machine@gmail.com>
2020-07-14 22:37:58 -0400

    Update new-member-roles.py

--- new-member-roles/new-member-roles.py
+++ new-member-roles/new-member-roles.py
@@ -9,16 +9,17 @@ class NewMemberRoles(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
-    @commands.command(name="approve", aliases=["app"])
+    @commands.command(name="newmember", aliases=["nmr"])
     @checks.has_permissions(PermissionLevel.MODERATOR)
-    async def approve(self, ctx):
+    async def newmember(self, ctx):
         member = ctx.guild.get_member(ctx.thread.recipient.id)
         pend = ctx.guild.get_role(324658636574162945)
         nego = ctx.guild.get_role(732405243299495968)
         madrid = ctx.guild.get_role(305440616354152450)
         await member.add_roles(nego, madrid)
         await member.remove_roles(pend)
-        await ctx.send("{member} has been successfully approved!")
+        
+        await ctx.send("This member has been successfully approved!")
         
         config = await self.db.find_one({"_id": "config"})
 
