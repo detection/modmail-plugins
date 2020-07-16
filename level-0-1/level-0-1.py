5f87acb82bad5205bc449456c1fb236903ba0daf
detection <big.machine@gmail.com>
2020-07-16 00:00:22 -0400

    Update level-0-1.py

--- level-0-1/level-0-1.py
+++ level-0-1/level-0-1.py
@@ -1,16 +1,14 @@
 import discord
 from discord.ext import commands
 
-client = discord.Client()
-
 class Level01(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
-        nego = client.guild.get_role(732405243299495968)
-        ns = client.guild.get_role(603767376852287502)
+        nego = guild.get_role(732405243299495968)
+        ns = guild.get_role(603767376852287502)
         if ns not in before.roles and ns in after.roles:
             await user.remove_roles(nego)
         else:
