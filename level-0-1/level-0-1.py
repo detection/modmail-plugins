e9ef101d1938b05573ee0c9e5a0a09e00cace1e8
detection <big.machine@gmail.com>
2020-07-15 23:47:55 -0400

    Update level-0-1.py

--- level-0-1/level-0-1.py
+++ level-0-1/level-0-1.py
@@ -6,7 +6,7 @@ class Level01(commands.Cog):
         self.bot = bot
     
     @commands.Cog.listener()
-    async def on_member_update(before, after):
+    async def on_member_update(self, before, after):
         nego = client.guild.get_role(732405243299495968)
         ns = client.guild.get_role(603767376852287502)
         if ns not in before.roles and ns in after.roles:
