267943114b5c3d9f7d9268ada5cf0c6e8e0ef750
detection <big.machine@gmail.com>
2020-07-15 21:04:17 -0400

    Update level-0-1.py

--- level-0-1/level-0-1.py
+++ level-0-1/level-0-1.py
@@ -9,10 +9,10 @@ class Level01(commands.Cog):
     async def on_member_update(before, after):
         nego = ctx.guild.get_role(732405243299495968)
         ns = ctx.guild.get_role(603767376852287502)
-        if after.roles === ns:
-          await user.remove_roles(nego)
-      else
-        return
+        if after.roles == ns:
+            await user.remove_roles(nego)
+        else
+            return
                
 def setup(bot):
     bot.add_cog(Level01(bot))
