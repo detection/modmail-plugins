6ccba7db087237d5f871c27e572760b208321dc3
detection <big.machine@gmail.com>
2020-09-09 21:54:12 -0400

    Update role-management.py

--- role-management/role-management.py
+++ role-management/role-management.py
@@ -26,6 +26,26 @@ class RoleManage(commands.Cog):
             await user.remove_roles(nego)
         elif red not in before.roles and red in after.roles:
             await user.remove_roles(orange, yellow, green, lime, seafoam, aqua, blue, lav, violet, indigo)
+        elif orange not in before.roles and orange in after.roles:
+            await user.remove_roles(red, yellow, green, lime, seafoam, aqua, blue, lav, violet, indigo)
+        elif yellow not in before.roles and yellow in after.roles:
+            await user.remove_roles(red, orange, green, lime, seafoam, aqua, blue, lav, violet, indigo)
+        elif green not in before.roles and green in after.roles:
+            await user.remove_roles(red, orange, yellow, lime, seafoam, aqua, blue, lav, violet, indigo)
+        elif lime not in before.roles and lime in after.roles:
+            await user.remove_roles(red, orange, yellow, green, seafoam, aqua, blue, lav, violet, indigo)
+        elif seafoam not in before.roles and seafoam in after.roles:
+            await user.remove_roles(red, orange, yellow, green, lime, aqua, blue, lav, violet, indigo)
+        elif aqua not in before.roles and aqua in after.roles:
+            await user.remove_roles(red, orange, yellow, green, lime, seafoam, blue, lav, violet, indigo)
+        elif blue not in before.roles and blue in after.roles:
+            await user.remove_roles(red, orange, yellow, green, lime, seafoam, aqua, lav, violet, indigo)
+        elif lav not in before.roles and lav in after.roles:
+            await user.remove_roles(red, orange, yellow, green, lime, seafoam, aqua, blue, violet, indigo)
+        elif violet not in before.roles and violet in after.roles:
+            await user.remove_roles(red, orange, yellow, green, lime, seafoam, aqua, blue, lav, indigo)
+        elif indigo not in before.roles and indigo in after.roles:
+            await user.remove_roles(red, orange, yellow, green, lime, seafoam, aqua, blue, lav, violet)
         else:
             return           
 def setup(bot):
