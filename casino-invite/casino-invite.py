97ee7b5a0bd245581947b4c1d69075f3fe7cdaf7
detection <big.machine@gmail.com>
2021-03-11 23:31:10 -0500

    Update casino-invite.py

--- casino-invite/casino-invite.py
+++ casino-invite/casino-invite.py
@@ -1,23 +1,4 @@
-import discord
-from discord.ext import commands
 
-class Level01(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-    
-    @commands.Cog.listener()
-    async def on_member_update(self, before, after):
-        guild = self.bot.get_guild(173554823633829888)
-        user = guild.get_member(after.id)
-        nego = guild.get_role(732405243299495968)
-        ns = guild.get_role(603767376852287502)
-        if ns not in before.roles and ns in after.roles:
-            await user.remove_roles(nego)
-        else:
-            return
-               
-def setup(bot):
-    bot.add_cog(Level01(bot))
 import discord
 from discord.ext import commands
 
