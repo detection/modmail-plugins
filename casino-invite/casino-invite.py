394308c0a8fd3ec209b8ef713517682412a19992
detection <big.machine@gmail.com>
2021-03-12 15:59:45 -0500

    Update casino-invite.py

--- casino-invite/casino-invite.py
+++ casino-invite/casino-invite.py
@@ -8,7 +8,7 @@ class CasinoInvite(commands.Cog):
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
         guild = self.bot.get_guild(173554823633829888)
-        user = guild.get_member(after.id)
+        member = guild.get_member(after.id)
         juvC = guild.get_role(603767565130399784)
         socios = guild.get_role(541375823395946522)
         if juvC not in before.roles and juvC in after.roles and socios not in after.roles:
