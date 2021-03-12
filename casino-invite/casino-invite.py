55765d645144a5bdae2cfa025f370554edb0bfb9
detection <big.machine@gmail.com>
2021-03-12 15:58:40 -0500

    Update casino-invite.py

--- casino-invite/casino-invite.py
+++ casino-invite/casino-invite.py
@@ -11,7 +11,6 @@ class CasinoInvite(commands.Cog):
         user = guild.get_member(after.id)
         juvC = guild.get_role(603767565130399784)
         socios = guild.get_role(541375823395946522)
-        config = await self.db.find_one({"_id": "ci-dm-config"})
         if juvC not in before.roles and juvC in after.roles and socios not in after.roles:
             print(f"{member.name} test dm message send")
             await member.send('test dm message')
