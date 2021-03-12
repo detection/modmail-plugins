483be884257984c42b416aa5fe6d9ac4ce2b93ea
detection <big.machine@gmail.com>
2021-03-12 16:40:12 -0500

    Update casino-invite.py

--- casino-invite/casino-invite.py
+++ casino-invite/casino-invite.py
@@ -11,10 +11,9 @@ class CasinoInvite(commands.Cog):
         member = guild.get_member(after.id)
         juvC = guild.get_role(603767565130399784)
         socios = guild.get_role(541375823395946522)
-        self_roles = guild.get_channel(800176964559306814)
         if juvC not in before.roles and juvC in after.roles and socios not in after.roles:
             print(f"{member.name} test dm message send")
-            await member.send("""Congratulations, you've reached Level 20 in the Real Madrid Discord server! Please accept this invitation to our exclusive Casino Royale, where we gamble our very own currency, the Flocoin.\nHead to""" + self_roles + """and tap the <:flocoin:541110660545773618> to enter. Good luck!""")
+            await member.send("""Congratulations, you've reached Level 20 in the Real Madrid Discord server! Please accept this invitation to our exclusive Casino Royale, where we gamble our very own currency, the Flocoin.\nHead to <#800176964559306814> and tap the <:flocoin:541110660545773618> to enter. Good luck!""")
         else:
             return
                
