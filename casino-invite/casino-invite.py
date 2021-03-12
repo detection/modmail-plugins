b496ddea809b30bd7eb9ebc947d8959e1d7b7b3b
detection <big.machine@gmail.com>
2021-03-12 15:50:38 -0500

    Update casino-invite.py

--- casino-invite/casino-invite.py
+++ casino-invite/casino-invite.py
@@ -5,16 +5,17 @@ class CasinoInvite(commands.Cog):
     
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
-        try:
-            guild = self.bot.get_guild(173554823633829888)
-            member = guild.get_member(after.id)
-            juvC = guild.get_role(603767565130399784)
-            socios = guild.get_role(541375823395946522)
-            if juvC not in before.roles and juvC in after.roles and socios not in after.roles:
-                print(f"{member.name} test dm message send")
-                await member.send('test dm message')
+        guild = self.bot.get_guild(173554823633829888)
+        user = guild.get_member(after.id)
+        juvC = guild.get_role(603767565130399784)
+        socios = guild.get_role(541375823395946522)
+        config = await self.db.find_one({"_id": "ci-dm-config"})
+        if juvC not in before.roles and juvC in after.roles and socios not in after.roles:
+            print(f"{member.name} test dm message send")
+            await member.send('test dm message')
         except AttributeError as e:
             print(e)
+            return
                
 def setup(bot):
     bot.add_cog(CasinoInvite(bot))
