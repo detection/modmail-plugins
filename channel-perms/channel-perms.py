54cd74c6f913b27248fb87917a19de50f31db08d
detection <big.machine@gmail.com>
2021-03-17 15:41:22 -0400

    Update channel-perms.py

--- channel-perms/channel-perms.py
+++ channel-perms/channel-perms.py
@@ -12,29 +12,30 @@ class ChannelPerms(commands.Cog):
     @checks.has_permissions(PermissionLevel.MODERATOR)
     async def viewNo(self, ctx):
         guild = self.bot.get_guild(173554823633829888)
-        role = guild.get_role(821435409397055528) #Madridistras! 
         await ctx.channel.set_permissions(ctx.guild.get_role(821435409397055528), read_messages=False)
+        await bot.delete_message(ctx.message)
         
     @commands.command(name="channelshow", aliases=["cshow"])
     @checks.has_permissions(PermissionLevel.MODERATOR)
     async def viewYes(self, ctx):
         guild = self.bot.get_guild(173554823633829888)
-        role = guild.get_role(821435409397055528) #Madridistras!
         await ctx.channel.set_permissions(ctx.guild.get_role(821435409397055528), read_messages=True)
+        await bot.delete_message(ctx.message)
         
     @commands.command(name="channelnotype", aliases=["cnotype"])
     @checks.has_permissions(PermissionLevel.MODERATOR)
     async def typeNo(self, ctx):
         guild = self.bot.get_guild(173554823633829888)
-        role = guild.get_role(821435409397055528) #Madridistras!
         await ctx.channel.set_permissions(ctx.guild.get_role(821435409397055528), send_messages=False)
+        await bot.delete_message(ctx.message)
         
     @commands.command(name="channeltype", aliases=["ctype"])
     @checks.has_permissions(PermissionLevel.MODERATOR)
     async def typeYes(self, ctx):
         guild = self.bot.get_guild(173554823633829888)
-        role = guild.get_role(821435409397055528) #Madridistras! 305440616354152450
+        #Madridistras! 305440616354152450
         await ctx.channel.set_permissions(ctx.guild.get_role(821435409397055528), send_messages=True)
+        await bot.delete_message(ctx.message)
                
 def setup(bot):
     bot.add_cog(ChannelPerms(bot))
