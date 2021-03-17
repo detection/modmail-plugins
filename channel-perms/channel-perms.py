f99c55b34d3b1af3798c0f75644b7c586fade6b1
detection <big.machine@gmail.com>
2021-03-17 15:51:50 -0400

    Update channel-perms.py

--- channel-perms/channel-perms.py
+++ channel-perms/channel-perms.py
@@ -13,21 +13,21 @@ class ChannelPerms(commands.Cog):
     async def viewNo(self, ctx):
         guild = self.bot.get_guild(173554823633829888)
         await ctx.channel.set_permissions(ctx.guild.get_role(821435409397055528), read_messages=False)
-        await self.delete_message(ctx.message)
+        await ctx.message.delete()
         
     @commands.command(name="channelshow", aliases=["cshow"])
     @checks.has_permissions(PermissionLevel.MODERATOR)
     async def viewYes(self, ctx):
         guild = self.bot.get_guild(173554823633829888)
         await ctx.channel.set_permissions(ctx.guild.get_role(821435409397055528), read_messages=True)
-        await self.delete_message(ctx.message)
+        await ctx.message.delete()
         
     @commands.command(name="channelnotype", aliases=["cnotype"])
     @checks.has_permissions(PermissionLevel.MODERATOR)
     async def typeNo(self, ctx):
         guild = self.bot.get_guild(173554823633829888)
         await ctx.channel.set_permissions(ctx.guild.get_role(821435409397055528), send_messages=False)
-        await self.delete_message(ctx.message)
+        await ctx.message.delete()
         
     @commands.command(name="channeltype", aliases=["ctype"])
     @checks.has_permissions(PermissionLevel.MODERATOR)
@@ -35,7 +35,7 @@ class ChannelPerms(commands.Cog):
         guild = self.bot.get_guild(173554823633829888)
         #Madridistras! 305440616354152450
         await ctx.channel.set_permissions(ctx.guild.get_role(821435409397055528), send_messages=True)
-        await self.delete_message(ctx.message)
+        await ctx.message.delete()
                
 def setup(bot):
     bot.add_cog(ChannelPerms(bot))
