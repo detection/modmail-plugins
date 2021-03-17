62a2a677a9c9eec35d9d99753e3f5c8af4e8f21e
detection <big.machine@gmail.com>
2021-03-17 15:36:31 -0400

    Update channel-perms.py

--- channel-perms/channel-perms.py
+++ channel-perms/channel-perms.py
@@ -13,28 +13,28 @@ class ChannelPerms(commands.Cog):
     async def viewNo(self, ctx):
         guild = self.bot.get_guild(173554823633829888)
         role = guild.get_role(821435409397055528) #Madridistras! 
-        await ctx.channel.set_permissions(ctx.guild.role, read_messages=False)
+        await ctx.channel.set_permissions(ctx.guild.get_role(821435409397055528), read_messages=False)
         
     @commands.command(name="channelshow", aliases=["cshow"])
     @checks.has_permissions(PermissionLevel.MODERATOR)
     async def viewYes(self, ctx):
         guild = self.bot.get_guild(173554823633829888)
         role = guild.get_role(821435409397055528) #Madridistras!
-        await ctx.channel.set_permissions(ctx.guild.role, read_messages=True)
+        await ctx.channel.set_permissions(ctx.guild.get_role(821435409397055528), read_messages=True)
         
     @commands.command(name="channelnotype", aliases=["cnotype"])
     @checks.has_permissions(PermissionLevel.MODERATOR)
     async def typeNo(self, ctx):
         guild = self.bot.get_guild(173554823633829888)
         role = guild.get_role(821435409397055528) #Madridistras!
-        await ctx.channel.set_permissions(ctx.guild.role, send_messages=False)
+        await ctx.channel.set_permissions(ctx.guild.get_role(821435409397055528), send_messages=False)
         
     @commands.command(name="channeltype", aliases=["ctype"])
     @checks.has_permissions(PermissionLevel.MODERATOR)
     async def typeYes(self, ctx):
         guild = self.bot.get_guild(173554823633829888)
         role = guild.get_role(821435409397055528) #Madridistras! 305440616354152450
-        await ctx.channel.set_permissions(ctx.guild.role, send_messages=True)
+        await ctx.channel.set_permissions(ctx.guild.get_role(821435409397055528), send_messages=True)
                
 def setup(bot):
     bot.add_cog(ChannelPerms(bot))
