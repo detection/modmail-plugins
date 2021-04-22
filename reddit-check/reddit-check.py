be1633d41f07842032978e8bc92e5e96d6907f21
detection <big.machine@gmail.com>
2021-04-22 09:50:15 -0400

    Update reddit-check.py

--- reddit-check/reddit-check.py
+++ reddit-check/reddit-check.py
@@ -8,31 +8,25 @@ class RedditCheck(commands.Cog):
         print('v1.0.0')
 
     @commands.Cog.listener()
-    async def on_guild_channel_create(self, channel):
-        rma_id = 649463797949399050
+    async def on_thread_ready(self, thread, creator, category, initial_message):
 
-        if channel.category.id == rma_id:
-            await asyncio.sleep(1)
-            messages = await channel.history().flatten()
-            new_channel = self.bot.get_channel(channel.id)
-            first_message = self.bot.get(initial_message)
-            first_line = first_message.partition('\n')[0]
-            skip_words = ['dont have', 'don\'t', 'dont use', 'no reddit', 'anonymous']
-            bad_chars = [' ', 'u/', "/u/", "1.", "1)", "#1"]
-            dash_first = ['-']
+        await asyncio.sleep(1)
+        print(initial_message)
+        new_channel = self.bot.get_channel(channel.id)
+        first_message = initial_message
+        first_line = first_message.partition('\n')[0]
+        skip_words = ['dont have', 'don\'t', 'dont use', 'no reddit', 'anonymous']
+        bad_chars = [' ', 'u/', "/u/", "1.", "1)", "#1"]
+        dash_first = ['-']
+
+        for i in bad_chars:
+          first_line = first_line.replace(i, '')
+
+        for i in dash_first:
+          if first_line[0] == "-":
+             first_line = first_line.replace(i, '', 1)
+
+        await new_channel.send('https://redditmetis.com/user/' + str(first_line))
             
-            for i in bad_chars:
-              first_line = first_line.replace(i, '')
-
-            for i in dash_first:
-              if first_line[0] == "-":
-                 first_line = first_line.replace(i, '', 1)
-                 
-            await new_channel.send('https://redditmetis.com/user/' + str(first_line))
-            
-        else:
-            print('Wrong category')
-            print(channel.category.name)
-
 def setup(bot):
     bot.add_cog(RedditCheck(bot))
