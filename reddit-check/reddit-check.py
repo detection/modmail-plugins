bb7f5fddc90572e91021172fd140ae9f8cfdafcb
detection <big.machine@gmail.com>
2021-04-22 10:28:25 -0400

    Update reddit-check.py

--- reddit-check/reddit-check.py
+++ reddit-check/reddit-check.py
@@ -10,11 +10,12 @@ class RedditCheck(commands.Cog):
     @commands.Cog.listener()
     async def on_thread_ready(self, thread, creator, category, initial_message):
 
-        await asyncio.sleep(1)
         print(initial_message)
         new_channel = self.bot.get_channel(thread.channel.id)
+        print(new_channel)
         first_message = initial_message
-        first_line = first_message.partition('\n')[0]
+        print(first_message)
+        first_line = first_message.splitlines()[0]
         skip_words = ['dont have', 'don\'t', 'dont use', 'no reddit', 'anonymous']
         bad_chars = [' ', 'u/', "/u/", "1.", "1)", "#1"]
         dash_first = ['-']
