157adb1de0875b1c748626580480379a1ed3987b
detection <big.machine@gmail.com>
2021-04-22 12:26:17 -0400

    Update reddit-check.py

--- reddit-check/reddit-check.py
+++ reddit-check/reddit-check.py
@@ -10,22 +10,31 @@ class RedditCheck(commands.Cog):
     @commands.Cog.listener()
     async def on_thread_ready(self, thread, creator, category, initial_message):
 
-        new_channel = self.bot.get_channel(thread.channel.id)
+        thread_channel = self.bot.get_channel(thread.channel.id)
         first_message = initial_message.content
-        print(first_message)
         first_line = first_message.splitlines()[0]
         skip_words = ['dont have', 'don\'t', 'dont use', 'no reddit', 'anonymous']
         bad_chars = [' ', 'u/', "/u/", "1.", "1)", "#1"]
         dash_first = ['-']
 
-        for i in bad_chars:
-          first_line = first_line.replace(i, '')
+        if any(i in first_line for i in skip_words):
+            skip_all = True
+        else:
+            skip_all = False
 
-        for i in dash_first:
-          if first_line[0] == "-":
-             first_line = first_line.replace(i, '', 1)
+        if skip_all == False:
+            if "," in first_line[0:20]:
+                before_comma = first_line.partition(',')
+                first_line = before_comma[0]
 
-        await new_channel.send('https://redditmetis.com/user/' + str(first_line))
+            for i in bad_chars:
+                first_line = first_line.replace(i, '')
+
+            for i in dash_first:
+                if first_line[0] == "-":
+                    first_line = first_line.replace(i, '', 1)
+
+        await thread_channel.send('https://redditmetis.com/user/' + str(first_line))
             
 def setup(bot):
     bot.add_cog(RedditCheck(bot))
