9eaa0eb3c069b094ea8d826b548dfb81b4441c73
detection <big.machine@gmail.com>
2021-04-22 14:17:03 -0400

    Update new-ping.py

--- new-ping/new-ping.py
+++ new-ping/new-ping.py
@@ -1,31 +1,16 @@
 from discord.ext import commands
 import discord
-import asyncio
 
 class NewPing(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        print('v2.0.0')
 
     @commands.Cog.listener()
-    async def on_guild_channel_create(self, channel):
-        rma_id = 649463797949399050
-        rmj_id = 719324997461606455
-
-        if channel.category.id == rma_id or rmj_id:
-            await asyncio.sleep(1)
-            messages = await channel.history().flatten()
-            newChannel = self.bot.get_channel(channel.id)
-            userID = newChannel.topic.split(': ')[1]
-            print('userID:', userID)
-
-            member = self.bot.get_user(int(userID))
-            print("Member: ", member)
-
-            await newChannel.send(str(member.mention))
-        else:
-            print('Wrong category')
-            print(channel.category.name)
+    async on_thread_ready(self, thread, creator, category, initial_message):
+        thread_channel = self.bot.get_channel(thread.channel.id)
+        member = self.bot.guild.get_member(thread.recipient.id)
+            
+        await thread_channel.send(str(member.mention))
 
 def setup(bot):
     bot.add_cog(NewPing(bot))
