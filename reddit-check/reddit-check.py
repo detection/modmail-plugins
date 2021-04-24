4fd07dab51abb013f0aacec2a21d0d48b8c877e1
detection <big.machine@gmail.com>
2021-04-24 15:13:10 -0400

    Update reddit-check.py

--- reddit-check/reddit-check.py
+++ reddit-check/reddit-check.py
@@ -12,7 +12,7 @@ class RedditCheck(commands.Cog):
         member = self.bot.guild.get_member(thread.recipient.id)
         pending = self.bot.guild.get_role(324658636574162945)
         first_line = initial_message.content.splitlines()[0]
-        skip_words = ['don\'t have', 'dont have', 'don\'t use', 'dont use', 'no reddit', 'anonymous', 'no have', 'dont reddit']
+        skip_words = ['don\'t have', 'dont have', 'don\'t use', 'dont use', 'no reddit', 'anonymous', 'no have', 'dont reddit', 'no tengo', 'not on']
         bad_chars = ['u/', '/', '1.', '1)', '#1', 'reddit username:', 'Reddit username:', 'Reddit Username:', ' ']
         dash_first = ['-']
 
