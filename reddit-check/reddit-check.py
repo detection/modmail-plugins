80e4e1dbe108d70d265edb15ec474d79198e4f19
detection <big.machine@gmail.com>
2021-04-23 23:05:17 -0400

    Update reddit-check.py

--- reddit-check/reddit-check.py
+++ reddit-check/reddit-check.py
@@ -13,7 +13,7 @@ class RedditCheck(commands.Cog):
         pending = self.bot.guild.get_role(324658636574162945)
         first_line = initial_message.content.splitlines()[0]
         skip_words = ['don\'t have', 'dont have', 'don\'t use', 'dont use', 'no reddit', 'anonymous', 'no have', 'dont reddit']
-        bad_chars = [' ', 'u/', '/', '1.', '1)', '#1', 'reddit username:']
+        bad_chars = [' ', 'u/', '/', '1.', '1)', '#1', 'reddit username:', 'Reddit username:', 'Reddit Username:']
         dash_first = ['-']
 
         if pending in member.roles:
@@ -26,7 +26,7 @@ class RedditCheck(commands.Cog):
                     before_period = first_line.partition('.')
                     first_line = before_period[0]
 
-                for i.lower() in bad_chars.lower():
+                for i in bad_chars:
                     first_line = first_line.replace(i, '')
 
                 for i in dash_first:
