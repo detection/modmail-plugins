b82328cc3bd6aad0a6293b7840bad602b525be7b
detection <big.machine@gmail.com>
2021-04-24 20:15:27 -0400

    Update reddit-check.py

--- reddit-check/reddit-check.py
+++ reddit-check/reddit-check.py
@@ -4,16 +4,13 @@ import discord
 class RedditCheck(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        print('v1.0.0')
 
     @commands.Cog.listener()
     async def on_thread_ready(self, thread, creator, category, initial_message):
-
         member = self.bot.guild.get_member(thread.recipient.id)
         pending = self.bot.guild.get_role(324658636574162945)
         first_line = initial_message.content.splitlines()[0]
-        print(first_line)
-        skip_words = ['don\'t', 'dont have', 'dont use', 'no reddit', 'anonymous', 'no have', 'dont reddit', 'no tengo', 'not on']
+        skip_words = ["don't", 'dont have', 'dont use', 'no reddit', 'anonymous', 'no have', 'dont reddit', 'no tengo', 'not on']
         bad_chars = ['u/', '/', '1.', '1)', '#1', 'reddit username:', 'Reddit username:', 'Reddit Username:', ' ']
         dash_first = ['-']
 
