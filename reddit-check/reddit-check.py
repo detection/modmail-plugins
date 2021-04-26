12922428373c50ee548b0e0816b072fad79c7be2
detection <big.machine@gmail.com>
2021-04-26 12:58:57 -0400

    Update reddit-check.py

--- reddit-check/reddit-check.py
+++ reddit-check/reddit-check.py
@@ -10,8 +10,8 @@ class RedditCheck(commands.Cog):
         member = self.bot.guild.get_member(thread.recipient.id)
         pending = self.bot.guild.get_role(324658636574162945)
         first_line = initial_message.content.splitlines()[0]
-        skip_words = ["don't", "don’t", "don`t", 'dont have', 'dont use', 'no reddit', 'anonymous', 'no have', 'dont reddit', 'no tengo', 'not on']
-        bad_chars = ['u/', '/', '1.', '1)', '#1', 'reddit username:', 'Reddit username:', 'Reddit Username:', '(', ' ']
+        skip_words = ["don't", "don’t", "don`t", "didn't", "didn’t", "didn`t", 'dont have', 'dont use', 'didnt have', 'didnt use', 'no reddit', 'anonymous', 'no have', 'dont reddit', 'didnt reddit', 'no tengo', 'not on']
+        bad_chars = ['u/', '/', '1.', '1)', '#1', 'reddit username', 'Reddit username', 'Reddit Username', ':', '(', ' ']
         dash_first = ['-']
 
         if pending in member.roles:
