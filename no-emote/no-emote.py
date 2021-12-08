39400e30880a9d20f047d2a12e4d27558df9d321
detection <big.machine@gmail.com>
2021-12-08 11:08:45 -0500

    Update no-emote.py

--- no-emote/no-emote.py
+++ no-emote/no-emote.py
@@ -7,12 +7,12 @@ class NoEmotes(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-    channel = self.bot.get_channel(payload.channel_id)
-    message = await channel.fetch_message(payload.message_id)
-    if channel == 305599363219062785:
-        await message.clear_reactions()
-    else:
-        pass
+        channel = self.bot.get_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+        if channel == 305599363219062785:
+            await message.clear_reactions()
+        else:
+            pass
 
 def setup(bot):
   bot.add_cog(NoEmotes(bot))
