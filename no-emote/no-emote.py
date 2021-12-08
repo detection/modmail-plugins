82c01fd7a1e61f64a8c17ba2257e9473bb0dbbf9
detection <big.machine@gmail.com>
2021-12-08 11:07:38 -0500

    Update no-emote.py

--- no-emote/no-emote.py
+++ no-emote/no-emote.py
@@ -2,17 +2,17 @@ from discord.ext import commands
 import discord
 
 class NoEmotes(commands.Cog):
-  def __init__(self, bot):
-    self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-  @commands.Cog.listener()
-  async def on_raw_reaction_add(self, payload):
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
     channel = self.bot.get_channel(payload.channel_id)
     message = await channel.fetch_message(payload.message_id)
     if channel == 305599363219062785:
-      await message.clear_reactions()
+        await message.clear_reactions()
     else:
-      pass
+        pass
 
 def setup(bot):
   bot.add_cog(NoEmotes(bot))
