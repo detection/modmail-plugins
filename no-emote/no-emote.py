6cb86242b1d46c569ee2ab48c6a8826c85e08ed4
detection <big.machine@gmail.com>
2021-12-08 00:16:48 -0500

    Update no-emote.py

--- no-emote/no-emote.py
+++ no-emote/no-emote.py
@@ -6,9 +6,11 @@ class NoEmotes(commands.Cog):
     self.bot = bot
 
   @commands.Cog.listener()
-  async def on_raw_reaction_add(self, payload):
-    if payload.channel_id == 305599363219062785:
-      await payload.message_id.clear_reactions()
+  async def on_raw_reaction_add(payload):
+    channel = bot.get_channel(payload.channel_id)
+    message = await channel.fetch_message(payload.message_id)
+    if channel == 305599363219062785:
+      await message.clear_reactions()
     else:
       pass
 
