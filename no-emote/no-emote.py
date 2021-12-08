bebb0f1740e360a7beb0efe4b6c1f046a4a926e4
detection <big.machine@gmail.com>
2021-12-07 23:17:21 -0500

    Create no-emote.py

---
+++ no-emote/no-emote.py
@@ -0,0 +1,14 @@
+from discord.ext import commands
+import discord
+
+class NoEmotes(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+        async def on_reaction_add(reaction, user):
+            if reaction.message.channel.id == 907310047548309514:
+                await reaction.remove(user)
+
+def setup(bot):
+    bot.add_cog(NoEmotes(bot))
