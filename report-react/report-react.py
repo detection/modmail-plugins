262d597aa2213d8e86f2f72d9646c8371716e3c9
detection <big.machine@gmail.com>
2023-08-01 10:15:23 -0400

    Update report-react.py

--- report-react/report-react.py
+++ report-react/report-react.py
@@ -8,7 +8,12 @@ class ReportReact(commands.Cog):
     @commands.Cog.listener()
     async def on_reaction_add(reaction, user):
         if reaction.emoji == '🆘':
-            
+            thread = await self.bot.threads.find_or_create(user)
+            genesis_msg = await thread.get_genesis_message()
+            ctx = await self.bot.get_context(genesis_msg)
+    
+            thread.reply("Thank you for your report, please reply with any additional information you think we need to proceed.", True) # Second argument makes the message anon
+            ctx.send(f"{user.mention} has reported an incident. Message link: {reaction.message.jump_url}") 
         else:
             return
                
