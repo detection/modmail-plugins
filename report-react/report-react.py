5f21ae77af246d5f7413bfb7a8f935351ffce19e
detection <big.machine@gmail.com>
2023-08-01 11:10:21 -0400

    Update report-react.py

--- report-react/report-react.py
+++ report-react/report-react.py
@@ -17,7 +17,7 @@ class ReportReact(commands.Cog):
 
             print(genesis_msg)
 
-            thread.reply("Thank you for your report, please reply with any additional information you think we need to proceed.", True) # Second argument makes the message anon
+            # thread.reply("Thank you for your report, please reply with any additional information you think we need to proceed.", True) # Second argument makes the message anon
             ctx.send(f"{user.mention} has reported an incident. Message link: {reaction.message.jump_url}")
         else:
             return
