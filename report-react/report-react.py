1ca03d1603df464ebbf20616d1044e4a57d3412c
detection <big.machine@gmail.com>
2023-08-01 11:00:26 -0400

    Update report-react.py

--- report-react/report-react.py
+++ report-react/report-react.py
@@ -6,7 +6,7 @@ import asyncio
 class ReportReact(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
         if reaction.emoji == '🆘':
@@ -15,12 +15,12 @@ class ReportReact(commands.Cog):
             genesis_msg = await thread.get_genesis_message()
             ctx = await self.bot.get_context(genesis_msg)
 
-            print(genesis_message)
-    
+            print(genesis_msg)
+
             thread.reply("Thank you for your report, please reply with any additional information you think we need to proceed.", True) # Second argument makes the message anon
-            ctx.send(f"{user.mention} has reported an incident. Message link: {reaction.message.jump_url}") 
+            ctx.send(f"{user.mention} has reported an incident. Message link: {reaction.message.jump_url}")
         else:
             return
-               
+
 async def setup(bot):
     await bot.add_cog(ReportReact(bot))
