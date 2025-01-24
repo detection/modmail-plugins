9cb4d071525e5edb34bdb2d19b1b2780b3f3dfd7
detection <big.machine@gmail.com>
2025-01-24 10:03:16 -0500

    Create verify-msg.py

---
+++ verify-msg/verify-msg.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+import discord
+
+class Verify(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_thread_ready(self, thread, creator, category, initial_message):
+      unverified = guild.get_role(1271869103564394611)
+      if unverified in creator.roles:
+        await thread.channel.send(str(";verify"))
+      else:
+        return
+        
+async def setup(bot):
+    await bot.add_cog(NewPing(bot))
