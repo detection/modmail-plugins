7a24a0ede5a204971509ef890c614fef5ac24268
detection <big.machine@gmail.com>
2025-01-26 08:51:20 -0500

    Update verify-msg.py

--- verify-msg/verify-msg.py
+++ verify-msg/verify-msg.py
@@ -8,7 +8,7 @@ class Verify(commands.Cog):
     @commands.Cog.listener()
     async def on_thread_ready(self, thread, creator, category, initial_message):
         guild = self.bot.get_guild(173554823633829888)
-        member = guild.fetch_member(thread.recipient.id)
+        member = await guild.fetch_member(thread.recipient.id)
         unverified = guild.get_role(1271869103564394611)
         if unverified in member.roles:
             await thread.channel.send(str(";verify"))
