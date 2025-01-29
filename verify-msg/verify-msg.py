0b4fba7fa34f1322cc9741be5cd4edf3a9724efe
detection <big.machine@gmail.com>
2025-01-29 09:27:01 -0500

    Update verify-msg.py

--- verify-msg/verify-msg.py
+++ verify-msg/verify-msg.py
@@ -8,7 +8,7 @@ class Verify(commands.Cog):
     @commands.Cog.listener()
     async def on_thread_ready(self, thread, creator, category, initial_message):
         guild = self.bot.get_guild(173554823633829888)
-        member = await guild.fetch_member(thread.recipient.id)
+        member = await guild.fetch_member(creator.id)
         unverified = guild.get_role(1271869103564394611)
         msg = "Hello! Please verify your account through the <@703886990948565003> prompt sent to your DMs.\nIf the link expired by the time you attempt verification, you can send yourself a new one by sending the `/verify` command in any channel within the server."
         ctx = await self.bot.get_context(initial_message)
@@ -16,7 +16,7 @@ class Verify(commands.Cog):
         ctx.thread = thr
         
         if unverified in member.roles:
-            await ctx.invoke(self.bot.get_command('areply'), msg=msg)
+            await ctx.invoke(self.bot.get_command('reply'), msg=msg)
             # await thread.reply("Hello! Please verify your account through the <@703886990948565003> prompt sent to your DMs.\nIf the link expired by the time you attempt verification, you can send yourself a new one by sending the `/verify` command in any channel within the server.")
         else:
             return
